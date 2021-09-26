0c73ca49e8a69dfb26850290c162d8f62a83a4c6
MOHAMED YASHAR <83939080+MohamedYashar@users.noreply.github.com>
2021-09-26 19:03:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,138 @@
 // Create elements and append inside body starts here
 
-const container1 = document.createElement('div')
 
+// DIV ELEMENT1 TO DISPLAY
+const smallcontainer = document.createElement("div")
+smallcontainer.innerHTML = "Search your GIT HUB Details with your name"
+smallcontainer.id = "smallcontainer"
+document.body.appendChild(smallcontainer);
+
+
+// DIV ELEMENT2 START HERE
+const container1 = document.createElement('div')
 container1.className = 'container'
 
-const container2 = document.createElement('div')
 
-container2.className = 'container'
 
-const container3 = document.createElement('div')
-
+// Create element and append here in container1
 const inputBox = document.createElement('input')
-
 document.body.appendChild(inputBox)
-
-const search = document.createElement('button')
-
-
-search.innerHTML = 'Search'
-
 container1.appendChild(inputBox)
+
+
+// Create element and append here in container1
+const search = document.createElement('button')
+search.innerHTML = 'Search'
 container1.appendChild(search)
 document.body.appendChild(container1)
 
 
-
-const bigbox = document.createElement("div")
-bigbox.id= "bigbox"
-
-bigbox.appendChild(container1,container2,container3)
-
-document.body.appendChild(bigbox)
-
-
-// Create elements and append inside body starts here
+// Async function starts here
 
 async function repo() {
 
     try {
         let user = inputBox.value
-    inputBox.value = ''
 
-    container2.innerHTML = ''
-    container3.innerHTML = ''
+        // to reset the input value reset() is used similar for other
+        inputBox.value = ''
+        container2.innerHTML = ''
+        container3.innerHTML = ''
 
-    const response = await fetch(`https://api.github.com/users/${user}/repos`)
-    
+        const response = await fetch(`https://api.github.com/users/${user}/repos`)
+        const data = await response.json();
 
-    const data = await response.json();
+        console.log(data)
 
-    console.log(data)
-
-    appendData(data)
-        
-    } catch (error) {
-        console.log(error, " unable to fetch data")
-        
-    }
+        // appendData funtion called here
+        appendData(data)
+            
+        } catch (error) {
+            console.log(error, " unable to fetch data")
+            
+        }
     
 
 }
 
+//  Append data functions  done with for loop
+
+
+            // DIV ELEMENT2 & 3 created HERE
+
+            const container2 = document.createElement('div')
+            container2.className = 'container'
+
+            const container3 = document.createElement('div')
+            container3.className = 'container'
+
+
 
 function appendData(data) {
 
-    const total_repos = document.createElement('h1')
+        // Elemets to insert inside container2  STARTS HERE
+        const total_repos = document.createElement('h1')
+        total_repos.innerHTML = `Total Repos : ${data.length}`
+        container2.appendChild(total_repos)
+        
+        // Elements to insert inside container 2
+        const img = document.createElement('img')
+        img.src = data[0].owner.avatar_url;
+        container2.appendChild(img)
 
-    total_repos.innerHTML = `Total Repos : ${data.length}`
+        document.body.appendChild(container2)
 
-    container3.appendChild(total_repos)
-    document.body.appendChild(container3)
-
-    const img = document.createElement('img')
-    const git_url = document.createElement('a')
-
-    img.src = data[0].owner.avatar_url
-
-    container3.appendChild(img)
+        // Elements to insert inside container2  ENDS HERE
 
 
 
     for (let i = 0; i < data.length; i++) {
 
-        const repo_url = document.createElement('a')
-        const fork_counts = document.createElement('h5')
+
+       
+
+
+        // CREATE ELEMENT TO PUT IN CONTAINER 3
+
+        const repo_url          = document.createElement('a')
+        const fork_counts       = document.createElement('h5')
         const stargazers_counts = document.createElement('h5')
-        const languages = document.createElement('h4')
-        const names = document.createElement('h3')
+        const languages         = document.createElement('h4')
+        const names             = document.createElement('h3')
 
-        const smallbox = document.createElement('div')
-        smallbox.className = 'smallbox'
+        
+        // ASSIGN VARIABLE VALUE WITHH ITERATING DATA
 
-        repo_url.href = data[i].html_url
-        repo_url.innerHTML = `Repo Url : ${data[i].html_url}`
-        fork_counts.innerHTML = `Forks count : ${data[i].forks_count}`
-        stargazers_counts.innerHTML = `Stars count : ${data[i].stargazers_count}`
-        languages.innerHTML = `Languages : ${data[i].language}` 
-        names.innerHTML =  ` Repo Name : ${data[i].name}`
+        repo_url.href               =   data[i].html_url
+        repo_url.innerHTML          =  `Repo Url    : ${data[i].html_url}`
+        fork_counts.innerHTML       =  `Forks count : ${data[i].forks_count}`
+        stargazers_counts.innerHTML =  `Stars count : ${data[i].stargazers_count}`
+        languages.innerHTML         =  `Languages   : ${data[i].language}` 
+        names.innerHTML             =  `Repo Name   : ${data[i].name}`
 
+         // CREATE A DIV ELEMENT TO APPEND IN INSIDE CONTAINER3 ELEMENT
+         const smallbox = document.createElement('div')
+         smallbox.className = 'smallbox'
+
+        // APPEND ASSIGNED VARIABLE IN A DIV ELEMENT
         smallbox.appendChild(names)
         smallbox.appendChild(languages)
         smallbox.appendChild(fork_counts)
         smallbox.appendChild(stargazers_counts)
         smallbox.appendChild(repo_url)
 
-        container2.appendChild(smallbox)
+        
+        // APPEND THE SMALLBOX (DIV ELEMENT)=>CONTAINER 3 ( DIV ELEMENT)
+        container3.appendChild(smallbox)
 
     }
-
-    document.body.appendChild(container2)
+        // APPEND  CONTAINER 3 TO DOM
+        document.body.appendChild(container3)
 
 
 }
 
-search.addEventListener('click', repo)
+        // ADD EVENT LISTNERS TO ACTIVATE CLICK BUTTON AND REPO (ASYNC FUNCTION)
+        search.addEventListener('click', repo)
+
+// code ends here//
